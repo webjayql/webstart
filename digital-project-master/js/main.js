15e60ec66bb55f88dccc97fa78b5ca3a9ae41f3c
Jay Pavlov <webjayql@gmail.com>
2020-05-23 08:57:03 +1000

    new section // project

--- digital-project-master/js/main.js
+++ digital-project-master/js/main.js
@@ -9,4 +9,22 @@ $(document).ready(function () {
     modalClose.on('click', function () {
         modal.removeClass('modal-visible');
     });
+    var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+      })
+      var next = $('.swiper-button-next');
+      var prev = $('.swiper-button-prev');
+      var bullets = $('.swiper-pagination');
+
+      next.css('left', prev.width() + 10 + bullets.width() +10 )
+      bullets.css('left', prev.width() + 10 )
 });
